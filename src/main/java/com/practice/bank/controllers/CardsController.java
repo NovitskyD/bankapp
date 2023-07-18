f2f0c64ee5f651506612dd76617c3a8f22c50a69
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-19 01:34:12 +0300

    Added card controller

---
+++ src/main/java/com/practice/bank/controllers/CardsController.java
@@ -0,0 +1,72 @@
+package com.practice.bank.controllers;
+
+import com.practice.bank.dto.*;
+import com.practice.bank.services.*;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/accounts/cards")
+@RequiredArgsConstructor
+public class CardsController {
+    private final CardService cardService;
+    private final PaymentSystemService paymentSystemService;
+    private final LimitService limitService;
+    private final CurrencyService currencyService;
+    private final LoanService loanService;
+
+    @GetMapping("/{id}/create")
+    public String showCreateCardForm(@PathVariable("id") String accountId, Model model){
+        CardDto cardDto = cardService.createCardDtoForForm(accountId);
+        List<PaymentSystemDto> paymentSystems = paymentSystemService.getAllPaymentSystems();
+        List<LimitDto> limits = limitService.getAllLimits();
+        List<CurrencyDto> currencies = currencyService.getAllCurrencies();
+        LoanDto loanDto = loanService.createLoanDtoForForm();
+
+        model.addAttribute("currencies", currencies);
+        model.addAttribute("card", cardDto);
+        model.addAttribute("paymentSystems", paymentSystems);
+        model.addAttribute("limits", limits);
+        model.addAttribute("loan", loanDto);
+        return "cards/create_card_form";
+    }
+
+    @PostMapping()
+    public String createCard(@ModelAttribute("card") @Valid CardDto cardDto,
+                            @ModelAttribute("loan") @Valid LoanDto loanDto,
+                            BindingResult result){
+        if(result.hasErrors()){
+            return "cards/create_card_form";
+        }
+        System.out.println(cardDto);
+        if("Credit".equals(cardDto.getAccount().getAccountType()) && loanDto != null) {
+            cardDto.setLoan(loanDto);
+        }
+        cardService.insertData(cardDto);
+        return "redirect:/accounts/" + cardDto.getAccount().getClient().getId();
+    }
+
+    @GetMapping("/top-up")
+    public String showTopUpForm(Model model){
+        List<CurrencyDto> currencies = currencyService.getAllCurrencies();
+        model.addAttribute("topUpDto", new TopUpDto());
+        model.addAttribute("currencies", currencies);
+        return "cards/top_up_form";
+    }
+
+    @PostMapping("/top-up")
+    public String processTopUpForm(@ModelAttribute("topUpDto") @Valid TopUpDto topUpDto, BindingResult result){
+        if (result.hasErrors()){
+            return "cards/top_up_form";
+        }
+        cardService.topUpBalance(topUpDto);
+
+        return "messages/success";
+    }
+}

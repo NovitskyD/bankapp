5902e86cc82ddfd1ba018303637077a95e0e48d8
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-26 01:14:18 +0300

    Add necessary methods for turning an Entity into a DTO and vice versa

--- src/main/java/com/practice/bank/mapper/DtoToEntity.java
+++ src/main/java/com/practice/bank/mapper/DtoToEntity.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 
 public class DtoToEntity {
     public static ClientEntity clientsDtoToEntity(ClientDto clientDto){
+        if(clientDto == null){
+            return null;
+        }
         return ClientEntity.builder()
                 .firstName(clientDto.getFirstName())
                 .lastName(clientDto.getLastName())
@@ -18,50 +21,75 @@ public class DtoToEntity {
                 .phone(clientDto.getPhone())
                 .email(clientDto.getEmail())
                 .password(clientDto.getPassword())
+                .role(clientDto.getRole())
+                .status(clientDto.getStatus())
                 .accounts(accountsDtoToEntityList(clientDto.getAccounts()))
+                .build();
+    }
+
+    public static ClientEntity clientsDtoToEntityForAccounts(ClientDto clientDto){
+        if(clientDto == null){
+            return null;
+        }
+        return ClientEntity.builder()
+                .firstName(clientDto.getFirstName())
+                .lastName(clientDto.getLastName())
+                .birthDate(clientDto.getBirthDate().atStartOfDay(ZoneId.systemDefault()).toInstant())
+                .address(clientDto.getAddress())
+                .phone(clientDto.getPhone())
+                .email(clientDto.getEmail())
+                .password(clientDto.getPassword())
+                .role(clientDto.getRole())
+                .status(clientDto.getStatus())
                 .build();
     }
 
     public static AccountEntity accountsDtoToEntityForClients(AccountDto accountDto){
         return AccountEntity.builder()
-//                .client(clientsDtoToEntity(accountDto.getClient()))
+                .client(clientsDtoToEntityForAccounts(accountDto.getClient()))
                 .accountNumber(accountDto.getAccountNumber())
                 .accountType(accountDto.getAccountType())
-                .balance(accountDto.getBalance())
                 .cards(cardsDtoToEntityList(accountDto.getCards()))
-                .loans(loansDtoToEntityList(accountDto.getLoans()))
-                .sentTransactions(transactionsDtoToEntityList(accountDto.getSentTransactions()))
-                .receivedTransactions(transactionsDtoToEntityList(accountDto.getReceivedTransactions()))
                 .build();
     }
 
     public static AccountEntity accountsDtoToEntity(AccountDto accountDto){
+        if(accountDto == null){
+            return null;
+        }
         return AccountEntity.builder()
                 .client(clientsDtoToEntity(accountDto.getClient()))
                 .accountNumber(accountDto.getAccountNumber())
                 .accountType(accountDto.getAccountType())
-                .balance(accountDto.getBalance())
                 .cards(cardsDtoToEntityList(accountDto.getCards()))
-                .loans(loansDtoToEntityList(accountDto.getLoans()))
-                .sentTransactions(transactionsDtoToEntityList(accountDto.getSentTransactions()))
-                .receivedTransactions(transactionsDtoToEntityList(accountDto.getReceivedTransactions()))
                 .build();
     }
 
     public static CardEntity cardsDtoToEntity(CardDto cardDto){
+        if(cardDto == null){
+            return null;
+        }
         return CardEntity.builder()
-                .account(accountsDtoToEntityForClients(cardDto.getAccount()))
+                .account(accountsDtoToEntity(cardDto.getAccount()))
+                .loan(loansDtoToEntity(cardDto.getLoan()))
                 .cardNumber(cardDto.getCardNumber())
                 .expirationDate(cardDto.getExpirationDate())
                 .holderName(cardDto.getHolderName())
                 .cvv(cardDto.getCvv())
+                .balance(cardDto.getBalance())
+                .currency(currencyDtoToEntity(cardDto.getCurrency()))
                 .status(cardDto.getStatus())
+                .limit(limitDtoToEntity(cardDto.getLimit()))
+                .paymentSystem(paymentSystemDtoToEntity(cardDto.getPaymentSystem()))
                 .build();
     }
 
     public static LoanEntity loansDtoToEntity(LoanDto loanDto){
+        if(loanDto == null){
+            return null;
+        }
         return LoanEntity.builder()
-                .account(accountsDtoToEntityForClients(loanDto.getAccount()))
+                .card(cardsDtoToEntityForLoan(loanDto.getCard()))
                 .loanAmount(loanDto.getLoanAmount())
                 .interestRate(loanDto.getInterestRate())
                 .termMonths(loanDto.getTermMonths())
@@ -71,13 +99,34 @@ public class DtoToEntity {
                 .build();
     }
 
+    public static CardEntity cardsDtoToEntityForLoan(CardDto cardDto){
+        if(cardDto == null){
+            return null;
+        }
+        return CardEntity.builder()
+                .account(accountsDtoToEntity(cardDto.getAccount()))
+//                .loan(loansDtoToEntityForCards(cardDto.getLoan()))
+                .cardNumber(cardDto.getCardNumber())
+                .expirationDate(cardDto.getExpirationDate())
+                .holderName(cardDto.getHolderName())
+                .cvv(cardDto.getCvv())
+                .balance(cardDto.getBalance())
+                .currency(currencyDtoToEntity(cardDto.getCurrency()))
+                .status(cardDto.getStatus())
+                .limit(limitDtoToEntity(cardDto.getLimit()))
+                .paymentSystem(paymentSystemDtoToEntity(cardDto.getPaymentSystem()))
+                .build();
+    }
+
+
+
     public static TransactionEntity transactionsDtoToEntity(TransactionDto transactionDto){
         return TransactionEntity.builder()
-                .senderAccount(accountsDtoToEntityForClients(transactionDto.getSenderAccount()))
-                .recipientAccount(accountsDtoToEntityForClients(transactionDto.getRecipientAccount()))
+                .senderCard(cardsDtoToEntity(transactionDto.getSenderCard()))
+                .recipientCard(cardsDtoToEntity(transactionDto.getRecipientCard()))
                 .type(transactionDto.getType())
                 .amount(transactionDto.getAmount())
-                .currency(transactionDto.getCurrency())
+                .currency(currencyDtoToEntity(transactionDto.getCurrency()))
                 .description(transactionDto.getDescription())
                 .timestamp(transactionDto.getTimestamp())
                 .status(transactionDto.getStatus())
@@ -136,6 +185,128 @@ public class DtoToEntity {
         }
         return transactionDtos.stream()
                 .map(DtoToEntity::transactionsDtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    public static CurrencyEntity currencyDtoToEntity(CurrencyDto currencyDto){
+        if(currencyDto == null){
+            return null;
+        }
+        return CurrencyEntity.builder()
+                .code(currencyDto.getCode())
+                .name(currencyDto.getName())
+                .build();
+    }
+
+    public static List<CurrencyEntity> currencyDtoToEntityList(List<CurrencyDto> currencyDtos){
+        if(currencyDtos == null){
+            return new ArrayList<>();
+        }
+        return currencyDtos.stream()
+                .map(DtoToEntity::currencyDtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    public static CurrencyRateEntity currencyRateDtoToEntity(CurrencyRateDto currencyRateDto){
+        if(currencyRateDto == null){
+            return null;
+        }
+        return CurrencyRateEntity.builder()
+                .baseCurrency(currencyDtoToEntity(currencyRateDto.getBaseCurrency()))
+                .targetCurrency(currencyDtoToEntity(currencyRateDto.getTargetCurrency()))
+                .rate(currencyRateDto.getRate())
+                .date(currencyRateDto.getDate())
+                .build();
+    }
+
+    public static List<CurrencyRateEntity> currencyRateDtoToEntityList(List<CurrencyRateDto> currencyRateDtos){
+        if(currencyRateDtos == null){
+            return new ArrayList<>();
+        }
+        return currencyRateDtos.stream()
+                .map(DtoToEntity::currencyRateDtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    public static CountryEntity countryDtoToEntity(CountryDto countryDto){
+        if(countryDto == null){
+            return null;
+        }
+        return CountryEntity.builder()
+                .name(countryDto.getName())
+                .build();
+    }
+
+    public static PaymentSystemEntity paymentSystemDtoToEntity(PaymentSystemDto paymentSystemDto){
+        if(paymentSystemDto == null){
+            return null;
+        }
+        List<PaymentSystemCountryEntity> countryEntities = new ArrayList<>();
+        if (paymentSystemDto.getCountries() != null) {
+            countryEntities = paymentSystemDto.getCountries().stream()
+                    .map(DtoToEntity::paymentSystemCountryDtoToEntity)
+                    .collect(Collectors.toList());
+        }
+
+        PaymentSystemEntity entity = PaymentSystemEntity.builder()
+                .name(paymentSystemDto.getName())
+                .description(paymentSystemDto.getDescription())
+                .logoUrl(paymentSystemDto.getLogoUrl())
+                .transactionFeePercentage(paymentSystemDto.getTransactionFeePercentage())
+                .countries(countryEntities)
+                .build();
+
+        for (PaymentSystemCountryEntity countryEntity : countryEntities) {
+            countryEntity.setPaymentSystem(entity);
+        }
+
+        return entity;
+    }
+
+    public static PaymentSystemCountryEntity paymentSystemCountryDtoToEntity(PaymentSystemCountryDto dto){
+        if (dto == null){
+            return null;
+        }
+        return PaymentSystemCountryEntity.builder()
+                .id(paymentSystemCountryIdDtoToEntity(dto.getId()))
+                .country(countryDtoToEntity(dto.getCountry()))
+                .build();
+    }
+
+    public static PaymentSystemCountryId paymentSystemCountryIdDtoToEntity(PaymentSystemCountryIdDto dto){
+        if (dto == null){
+            return null;
+        }
+        return new PaymentSystemCountryId(Long.valueOf(dto.getPaymentSystemId()), Long.valueOf(dto.getCountryId()));
+    }
+
+    public static LimitEntity limitDtoToEntity(LimitDto limitDto){
+        if (limitDto == null){
+            return null;
+        }
+        return LimitEntity.builder()
+                .dailyLimit(limitDto.getDailyLimit())
+                .monthlyLimit(limitDto.getMonthlyLimit())
+                .transactionLimit(limitDto.getTransactionLimit())
+                .cards(cardsDtoToEntityList(limitDto.getCard()))
+                .build();
+    }
+
+    public static List<PaymentSystemEntity> paymentSystemDtoToEntityList(List<PaymentSystemDto> paymentSystemDtos){
+        if(paymentSystemDtos == null){
+            return new ArrayList<>();
+        }
+        return paymentSystemDtos.stream()
+                .map(DtoToEntity::paymentSystemDtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    public static List<LimitEntity> limitToDtoList(List<LimitDto> limitDtos){
+        if(limitDtos == null){
+            return new ArrayList<>();
+        }
+        return limitDtos.stream()
+                .map(DtoToEntity::limitDtoToEntity)
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/com/practice/bank/mapper/EntityToDto.java
+++ src/main/java/com/practice/bank/mapper/EntityToDto.java
@@ -10,6 +10,10 @@ import java.util.stream.Collectors;
 
 public class EntityToDto {
     public static ClientDto clientsEntityToDto(ClientEntity clientEntity){
+        if(clientEntity == null){
+            return null;
+        }
+
         return ClientDto.builder()
                 .id(clientEntity.getId().toString())
                 .firstName(clientEntity.getFirstName())
@@ -19,54 +23,114 @@ public class EntityToDto {
                 .phone(clientEntity.getPhone())
                 .email(clientEntity.getEmail())
                 .password(clientEntity.getPassword())
+                .role(clientEntity.getRole())
+                .status(clientEntity.getStatus())
                 .accounts(accountsEntitiesToDtoList(clientEntity.getAccounts()))
                 .build();
     }
 
+    public static ClientDto clientsEntityToDtoForAccounts(ClientEntity clientEntity){
+        if(clientEntity == null){
+            return null;
+        }
+
+        return ClientDto.builder()
+                .id(clientEntity.getId().toString())
+                .firstName(clientEntity.getFirstName())
+                .lastName(clientEntity.getLastName())
+                .birthDate(clientEntity.getBirthDate().atZone(ZoneId.systemDefault()).toLocalDate())
+                .address(clientEntity.getAddress())
+                .phone(clientEntity.getPhone())
+                .email(clientEntity.getEmail())
+                .password(clientEntity.getPassword())
+                .role(clientEntity.getRole())
+                .status(clientEntity.getStatus())
+                .build();
+    }
+
     public static AccountDto accountsEntityToDtoForClients(AccountEntity accountEntity){
+        if(accountEntity == null){
+            return null;
+        }
         return AccountDto.builder()
                 .id(accountEntity.getId().toString())
-//                .client(clientsEntityToDto(accountEntity.getClient()))
+                .client(clientsEntityToDtoForAccounts(accountEntity.getClient()))
                 .accountNumber(accountEntity.getAccountNumber())
                 .accountType(accountEntity.getAccountType())
-                .balance(accountEntity.getBalance())
                 .cards(cardsEntitiesToDtoList(accountEntity.getCards()))
-                .loans(loansEntityToDtoList(accountEntity.getLoans()))
-                .sentTransactions(transactionsEntityToDtoList(accountEntity.getSentTransactions()))
-                .receivedTransactions(transactionsEntityToDtoList(accountEntity.getReceivedTransactions()))
                 .build();
     }
 
     public static AccountDto accountsEntityToDto(AccountEntity accountEntity){
+        if(accountEntity == null){
+            return null;
+        }
         return AccountDto.builder()
                 .id(accountEntity.getId().toString())
                 .client(clientsEntityToDto(accountEntity.getClient()))
                 .accountNumber(accountEntity.getAccountNumber())
                 .accountType(accountEntity.getAccountType())
-                .balance(accountEntity.getBalance())
                 .cards(cardsEntitiesToDtoList(accountEntity.getCards()))
-                .loans(loansEntityToDtoList(accountEntity.getLoans()))
-                .sentTransactions(transactionsEntityToDtoList(accountEntity.getSentTransactions()))
-                .receivedTransactions(transactionsEntityToDtoList(accountEntity.getReceivedTransactions()))
+                .build();
+    }
+
+    public static AccountDto accountsEntityToDtoForCards(AccountEntity accountEntity){
+        if(accountEntity == null){
+            return null;
+        }
+        return AccountDto.builder()
+                .id(accountEntity.getId().toString())
+                .client(clientsEntityToDtoForAccounts(accountEntity.getClient()))
+                .accountNumber(accountEntity.getAccountNumber())
+                .accountType(accountEntity.getAccountType())
+//                .cards(cardsEntitiesToDtoList(accountEntity.getCards()))
                 .build();
     }
 
     public static CardDto cardsEntityToDto(CardEntity cardEntity){
         return CardDto.builder()
                 .id(cardEntity.getId().toString())
-                .account(accountsEntityToDtoForClients(cardEntity.getAccount()))
+                .account(accountsEntityToDtoForCards(cardEntity.getAccount()))
+                .loan(loansEntityToDto(cardEntity.getLoan()))
                 .cardNumber(cardEntity.getCardNumber())
                 .expirationDate(cardEntity.getExpirationDate())
                 .holderName(cardEntity.getHolderName())
                 .cvv(cardEntity.getCvv())
+                .balance(cardEntity.getBalance())
+                .currency(currencyEntityToDto(cardEntity.getCurrency()))
                 .status(cardEntity.getStatus())
+                .limit(limitEntityToDto(cardEntity.getLimit()))
+                .paymentSystem(paymentSystemEntityToDto(cardEntity.getPaymentSystem()))
+                .build();
+    }
+
+    public static CardDto cardsEntityToDtoForLoan(CardEntity cardEntity){
+        if(cardEntity == null){
+            return null;
+        }
+        return CardDto.builder()
+                .id(cardEntity.getId().toString())
+                .account(accountsEntityToDtoForCards(cardEntity.getAccount()))
+//                .loan(loansEntityToDto(cardEntity.getLoan()))
+                .cardNumber(cardEntity.getCardNumber())
+                .expirationDate(cardEntity.getExpirationDate())
+                .holderName(cardEntity.getHolderName())
+                .cvv(cardEntity.getCvv())
+                .balance(cardEntity.getBalance())
+                .currency(currencyEntityToDto(cardEntity.getCurrency()))
+                .status(cardEntity.getStatus())
+                .limit(limitEntityToDto(cardEntity.getLimit()))
+                .paymentSystem(paymentSystemEntityToDto(cardEntity.getPaymentSystem()))
                 .build();
     }
 
     public static LoanDto loansEntityToDto(LoanEntity loanEntity){
+        if(loanEntity == null){
+            return null;
+        }
         return LoanDto.builder()
                 .id(loanEntity.getId().toString())
-                .account(accountsEntityToDtoForClients(loanEntity.getAccount()))
+                .card(cardsEntityToDtoForLoan(loanEntity.getCard()))
                 .loanAmount(loanEntity.getLoanAmount())
                 .interestRate(loanEntity.getInterestRate())
                 .termMonths(loanEntity.getTermMonths())
@@ -79,11 +143,11 @@ public class EntityToDto {
     public static TransactionDto transactionsEntityToDto(TransactionEntity transactionEntity){
         return TransactionDto.builder()
                 .id(transactionEntity.getId().toString())
-                .senderAccount(accountsEntityToDtoForClients(transactionEntity.getSenderAccount()))
-                .recipientAccount(accountsEntityToDtoForClients(transactionEntity.getRecipientAccount()))
+                .senderCard(cardsEntityToDto(transactionEntity.getSenderCard()))
+                .recipientCard(cardsEntityToDto(transactionEntity.getRecipientCard()))
                 .type(transactionEntity.getType())
                 .amount(transactionEntity.getAmount())
-                .currency(transactionEntity.getCurrency())
+                .currency(currencyEntityToDto(transactionEntity.getCurrency()))
                 .description(transactionEntity.getDescription())
                 .timestamp(transactionEntity.getTimestamp())
                 .status(transactionEntity.getStatus())
@@ -145,4 +209,156 @@ public class EntityToDto {
                 .map(EntityToDto::transactionsEntityToDto)
                 .collect(Collectors.toList());
     }
+
+    public static CurrencyDto currencyEntityToDto(CurrencyEntity currencyEntity){
+        if(currencyEntity == null){
+            return null;
+        }
+        return CurrencyDto.builder()
+                .id(currencyEntity.getId().toString())
+                .code(currencyEntity.getCode())
+                .name(currencyEntity.getName())
+                .build();
+    }
+
+    public static List<CurrencyDto> currencyEntityToDtoList(List<CurrencyEntity> currencyEntities){
+        if(currencyEntities == null){
+            return new ArrayList<>();
+        }
+        return currencyEntities.stream()
+                .map(EntityToDto::currencyEntityToDto)
+                .collect(Collectors.toList());
+    }
+
+    public static CurrencyRateDto currencyRateDtoToEntity(CurrencyRateEntity currencyRateEntity){
+        if(currencyRateEntity == null){
+            return null;
+        }
+        return CurrencyRateDto.builder()
+                .id(currencyRateEntity.getId().toString())
+                .baseCurrency(currencyEntityToDto(currencyRateEntity.getBaseCurrency()))
+                .targetCurrency(currencyEntityToDto(currencyRateEntity.getTargetCurrency()))
+                .rate(currencyRateEntity.getRate())
+                .date(currencyRateEntity.getDate())
+                .build();
+    }
+
+    public static List<CurrencyRateDto> currencyRateDtoToEntityList(List<CurrencyRateEntity> currencyRateEntities){
+        if(currencyRateEntities == null){
+            return new ArrayList<>();
+        }
+        return currencyRateEntities.stream()
+                .map(EntityToDto::currencyRateDtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    public static CountryDto countryEntityToDto(CountryEntity countryEntity){
+        if (countryEntity == null){
+            return null;
+        }
+        return CountryDto.builder()
+                .id(countryEntity.getId().toString())
+                .name(countryEntity.getName())
+                .build();
+    }
+
+    public static PaymentSystemDto paymentSystemEntityToDto(PaymentSystemEntity entity){
+        if(entity == null){
+            return null;
+        }
+        List<PaymentSystemCountryDto> countryDtos = new ArrayList<>();
+        if(entity.getCountries() != null) {
+            countryDtos = entity.getCountries().stream()
+                    .map(EntityToDto::paymentSystemCountryEntityToDto)
+                    .collect(Collectors.toList());
+        }
+
+             PaymentSystemDto dto = PaymentSystemDto.builder()
+                .id(entity.getId().toString())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .logoUrl(entity.getLogoUrl())
+                .transactionFeePercentage(entity.getTransactionFeePercentage())
+                .countries(countryDtos)
+                .build();
+
+        for (PaymentSystemCountryDto countryDto : countryDtos){
+            countryDto.setPaymentSystem(dto);
+        }
+        return dto;
+    }
+
+    public static PaymentSystemCountryDto paymentSystemCountryEntityToDto(PaymentSystemCountryEntity entity){
+        if(entity == null){
+            return null;
+        }
+
+        return PaymentSystemCountryDto.builder()
+                .id(paymentSystemCountryIdEntityToDto(entity.getId()))
+                .country(countryEntityToDto(entity.getCountry()))
+                .build();
+    }
+
+    public static PaymentSystemCountryIdDto paymentSystemCountryIdEntityToDto(PaymentSystemCountryId entity){
+        if(entity == null){
+            return null;
+        }
+        return new PaymentSystemCountryIdDto(entity.getPaymentSystemId().toString(), entity.getCountryId().toString());
+    }
+
+    public static CardDto cardsEntityToDtoForLimit(CardEntity cardEntity){
+        return CardDto.builder()
+                .id(cardEntity.getId().toString())
+//                .account(accountsEntityToDtoForClients(cardEntity.getAccount()))
+                .cardNumber(cardEntity.getCardNumber())
+                .expirationDate(cardEntity.getExpirationDate())
+                .holderName(cardEntity.getHolderName())
+                .cvv(cardEntity.getCvv())
+                .balance(cardEntity.getBalance())
+                .status(cardEntity.getStatus())
+//                .limit(limitEntityToDto(cardEntity.getLimit()))
+                .paymentSystem(paymentSystemEntityToDto(cardEntity.getPaymentSystem()))
+                .build();
+    }
+
+    public static List<CardDto> cardsEntitiesToDtoListForLimit(List<CardEntity> cardEntities){
+        if(cardEntities == null){
+            return new ArrayList<>();
+        }
+        return cardEntities.stream()
+                .map(EntityToDto::cardsEntityToDtoForLimit)
+                .collect(Collectors.toList());
+    }
+
+    public static LimitDto limitEntityToDto(LimitEntity limitEntity){
+        if(limitEntity == null){
+            return null;
+        }
+        return LimitDto.builder()
+                .id(limitEntity.getId().toString())
+                .dailyLimit(limitEntity.getDailyLimit())
+                .monthlyLimit(limitEntity.getMonthlyLimit())
+                .transactionLimit(limitEntity.getTransactionLimit())
+                .card(cardsEntitiesToDtoListForLimit(limitEntity.getCards()))
+                .build();
+    }
+
+    public static List<PaymentSystemDto> paymentSystemEntityToDtoList(List<PaymentSystemEntity> paymentSystemEntities){
+        if(paymentSystemEntities == null){
+            return new ArrayList<>();
+        }
+        return paymentSystemEntities.stream()
+                .map(EntityToDto::paymentSystemEntityToDto)
+                .collect(Collectors.toList());
+    }
+
+    public static List<LimitDto> limitToDtoList(List<LimitEntity> limitEntities){
+        if(limitEntities == null){
+            return new ArrayList<>();
+        }
+        return limitEntities.stream()
+                .map(EntityToDto::limitEntityToDto)
+                .collect(Collectors.toList());
+    }
 }
+
